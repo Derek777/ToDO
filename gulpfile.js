83edc705ccfcd4fddbd1a944fd0d661507cbae15
Derek777 <vvcompany777@gmail.com>
2018-02-24 22:53:45 +0200

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var
+    gulp        = require("gulp"),
+    livereload  = require("gulp-livereload");
+
+gulp.task("reload", function () {
+    gulp.src('./style.css')
+        .pipe(livereload());
+});
+
+gulp.task("default", function () {
+   livereload.listen();
+   gulp.watch('./*.css' ["reload"]);
+});
