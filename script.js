b5357c9712b6927df1bd30ce40f553a559d8cae5
Derek777 <vvcompany777@gmail.com>
2018-03-02 23:43:19 +0200

    write empty-hidden

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var finishedTasks = document.getElementById('finished-tasks');
 
 function createNewTask(task) {
     var taskItem = document.createElement('li');
-    taskItem.className = "form-inline";
+    taskItem.className = "form-check-inline task";
     var check = document.createElement('input');
     check.type = "checkbox";
     check.className = "form-check-input";
@@ -14,7 +14,7 @@ function createNewTask(task) {
     label.innerText = task;
     var input =document.createElement('input');
     input.type = "text";
-    input.className = "form-control col-5 hidden";
+    input.className = "form-control hidden";
     var editBtn = document.createElement('button');
     editBtn.className = "btn btn-primary edit";
     editBtn.innerText = "Редагувати";
@@ -33,24 +33,32 @@ function createNewTask(task) {
 }
 
 function addtask() {
+    var empty = unfinishedTasks.querySelector('.empty');
     if (inputTask.value){
         var taskItem = createNewTask(inputTask.value);
+        if(empty){
+            empty.className = "hidden";
+        }
         unfinishedTasks.appendChild(taskItem);
         bindTaskEvents(taskItem, finishTask);
-        console.log(taskItem);
+        // console.log(taskItem);
         inputTask.value = "";
     }
 }
 
 function editTask() {
     var taskItem = this.parentNode;                   //li
+    var editBtn = taskItem.querySelector('.edit');  //Edit Btn
     var label = taskItem.querySelector('label'); // label
     var text = label.textContent;                //text in label
     var input = taskItem.querySelector('input[type=text]');    //input
     if(label.classList.contains('toEdit')){
         input.value = text;
+        editBtn.innerText = "Зберегти";
+
     }else{
         label.innerText = input.value;
+        editBtn.innerText = "Редагувати";
     }
 
     changeClass(label, input);
@@ -65,21 +73,49 @@ function deleteTask() {
     var taskItem = this.parentNode;
     var ul = taskItem.parentNode;
     ul.removeChild(taskItem);
+    if(!ul.querySelector('.task')){
+        ul.querySelector('.hidden').className = "toEdit empty";
+    }
+
 }
 
 function finishTask() {
     var taskItem = this.parentNode;
+    var editBtn = taskItem.querySelector('.edit');
+    var empty = finishedTasks.querySelector('.empty');
+
     finishedTasks.appendChild(taskItem);
     bindTaskEvents(taskItem, unfinishTask);
-
-    // console.log(taskItem);
+    taskItem.removeChild(editBtn);
+    if(!unfinishedTasks.querySelector('.task')){
+        unfinishedTasks.querySelector('.hidden').className = "toEdit empty";
+    }
+    if(empty){
+        empty.className = 'hidden';
+    }
 }
 
 function unfinishTask() {
     var taskItem = this.parentNode;
+    var hidden = unfinishedTasks.querySelector('.hidden');
+    var editBtn = document.createElement('button');
+    editBtn.className = "btn btn-primary edit";
+    editBtn.innerText = "Редагувати";
+
+    var deleteBtn = taskItem.querySelector('.delete');
+
+    taskItem.insertBefore(editBtn, deleteBtn);
+
     unfinishedTasks.appendChild(taskItem);
     // console.log('2');
     bindTaskEvents(taskItem, finishTask);
+    if(!finishedTasks.querySelector('.task')){
+        finishedTasks.querySelector('.hidden').className = "toEdit empty";
+    }
+    if(!hidden){
+        unfinishedTasks.querySelector('.empty').className = 'hidden';
+    }
+
 }
 
 function bindTaskEvents(tastItem, checkboxEvent) {
